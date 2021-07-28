1674e3ac056293a7cfd0b9b5781efe41f7bcab55
Eldad Marom <87889618+eldadmarom@users.noreply.github.com>
2021-07-28 17:04:41 +0300

    Update def_distance.py
    Bug fixed

--- def_distance.py
+++ def_distance.py
@@ -1,19 +1,17 @@
 def distance(num1, num2, num3):
-    if abs(num1 - num2) == 1 and abs(num3 - num1) >= 2:
+    num1 = int(num1)
+    num2 = int(num2)
+    num3 = int(num3)
+    con1 = abs(num1 - num2) == 1 and abs(num1 - num3) >= 2 and abs(num2 - num3) >= 2
+    con2 = abs(num1 - num3) == 1 and abs(num1 - num2) >= 2 and abs(num2 - num3) >= 2
+    if con1 or con2:
         print("True")
-    if abs(num1 - num3) == 1 and abs(num2 - num1) >= 2:
-        print("True")
-    if abs(num2 - num3) >= 2 and abs(num3 - num1) == 1:
-        print("True")
-    if abs(num3 - num2) >= 2 and abs(num2 - num1) == 1:
-        print("True")
-  #  else:
-    #    print("false")
-    if abs(num2) - abs(num1) == 0:
-        print("false")
-    if abs(num3) - abs(num1) == 0:
-        print("false")
-    if abs(num2) - abs(num3) == 0:
-        print("false")
-    
-distance (11,1,10)
+    else:
+        print("False")
+    return 
+
+num1 = abs(int(input("Enter the first number: ")))
+num2 = abs(int(input("enter the second number: ")))
+num3 = abs(int(input("enter the third number: ")))
+
+distance(num1, num2, num3)
